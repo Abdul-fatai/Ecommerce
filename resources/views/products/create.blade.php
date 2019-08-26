826ea3b53b361f7cc32cde9cee39c837d9262fe3
Abdul-fatai <staticdev20046@gmail.com>
2019-08-26 18:18:30 +0100

    new update

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Create Product</div>
+
+                <div class="card-body">
+                   <form action="{{ route('products.store') }}" method="POST">
+                       {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Price</label>
+                            <input type="number" name="price" class="form-control" value="{{ old('price') }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Image</label>
+                            <input type="file" name="image" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Description</label>
+                            <textarea name="description" class="form-control" cols="30" rows="10">{{ old('description')}}</textarea>
+                        </div>
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-primary">Create Product</button>
+                        </div>
+                   </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,16 +5,27 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">Products</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <table class="table hover">
+                        <thead>
+                          <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Delete</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row"></th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                          </tr>
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
